ea8e3d069fc7467d8a8ecfe5f54a9c7a4e9d29a3
Rexjurgoran <marvinhenssler@aol.com>
2022-02-18 12:07:10 +0100

    use properties

---
+++ properties.js
@@ -0,0 +1,13 @@
+const characterName = "Boris Lindor";
+const stats = [
+    {
+        "stat":     "ST",
+        "value":    36
+    }
+];
+const classIcon = "<i class='fa-solid fa-hat-wizard'></i>";
+const hpMax = 17;
+const hpCurrent = 17;
+const apMax = 18;
+const apCurrent = 18;
+const level = 3;
--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
+window.onload = start();
+
+function start(){
+    document.getElementById("character-name").innerText = characterName;   
+    document.getElementById("class-icon").innerHTML = classIcon;
+    document.getElementById("hp-bar").value = hpCurrent;
+    document.getElementById("hp-bar").max = hpMax;
+    document.getElementById("ap-bar").value = apCurrent;
+    document.getElementById("ap-bar").max = apMax;
+
+    let statContainer = "";
+    for(const pair of stats){
+        statContainer = statContainer + "<div class='pair'><div class='stat'>"+pair.stat+"</div><div class='value'>"+pair.value+"</div></div>"
+    }
+    document.getElementById("stat-container").innerHTML = statContainer;
+
+    let levelContainer = "";
+    for(let i = 0; i < level; i++) {
+        levelContainer = levelContainer + "<i class='fas fa-dice-d20'></i>";
+    }
+    document.getElementById("level").innerHTML = levelContainer;
+}
+
 function hideCard() {
     if (document.getElementById("card").style.display == "none") {
         $("#card").slideDown();
@@ -5,12 +28,15 @@ function hideCard() {
         $("#card").slideUp();
     }
 }
-function damage(barId) {
-    document.getElementById(barId).value += -1;
+
+function damage(bar) {
+    document.getElementById(bar+"-bar").value += -1;
 }
-function health(barId) {
-    document.getElementById(barId).value += 1;
+
+function health(bar) {
+    document.getElementById(bar+"-bar").value += 1;
 }
+
 function showStatContainer(){
     if (document.getElementById("stat-container").style.display == "none") {
         $("#stat-container").animate({width: 'toggle'});
