c164195b82e8bae1541589b83e661e24aba77364
Rexjurgoran <marvinhenssler@aol.com>
2022-06-10 21:16:22 +0200

    Additional stats

--- properties.js
+++ properties.js
@@ -2,12 +2,32 @@ const characterName = "Boris Lindor";
 const stats = [
     {
         "stat":     "ST",
-        "value":    36
+        "value":    40
+    },
+    {
+        "stat":     "GS",
+        "value":    86
+    },
+    {
+        "stat":     "GW",
+        "value":    35
+    },
+    {
+        "stat":     "KO",
+        "value":    84
+    },
+    {
+        "stat":     "IN",
+        "value":    94
+    },
+    {
+        "stat":     "ZT",
+        "value":    96
     }
 ];
 const classIcon = "<i class='fa-solid fa-hat-wizard'></i>";
 const hpMax = 17;
 const hpCurrent = 17;
-const apMax = 18;
-const apCurrent = 18;
-const level = 3;
+const apMax = 26;
+const apCurrent = 26;
+const level = 5;
